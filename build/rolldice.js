f1c0016924526576d36f59343f739a9c9dc5f7e0
Kevin Becker <kevinfjbecker@gmail.com>
2020-04-27 00:34:26 +0200

    feat: some work on combat simulation

--- build/rolldice.js
+++ build/rolldice.js
@@ -1,6 +1,18 @@
+
+/*
+    Matt McLimore / Elrond Hubbard (Elf Ranger)
+    Igor Olman / Topdeck (Drawf Warrior)
+    Ken Mills / Brother Zark (Human Cleric)
+    Carson the Muskrat / Lumpin Lightfingers (Muskrat Thief)
+    Gilly Woods the Perky Goth / Laeoretha (Human Witch)
+    Kayleigh Hardcastle / Amazona (Human Warrior)
+*/
+
 const d = sides => () => Math.round(Math.random() * sides) + 1;
 
-const d6 = d(6)
+const d6 = d(6);
+const d8 = d(8);
+const d20 = d(20);
 
 const ndx = (n, dx) => {
     const dice = Array(n).fill(0).map(() => dx());
@@ -12,9 +24,71 @@ const ndx = (n, dx) => {
         total: total,
         toString: toString,
         valueOf: valueOf
+    };
+};
+
+const rollInitiative = (character) => d20() + character.initiative;
+const rollAttack = (attacker, defender) => {
+    return d20() + attacker.attackBonus > defender.armorClass;
+};
+
+goblin = {
+    name: 'goblin',
+    initiative: 2,
+    hitPoints: 7,
+    armorClass: 15,
+    attackBonus: 4,
+    dmg: () => d6() + 2
+};
+
+cleric = {
+    name: 'cleric',
+    initiative: -1,
+    hitPoints: 7,
+    armorClass: 18,
+    attackBonus: 4,
+    dmg: () => d8() + 2
+};
+
+// todo: these should be actions & stuff
+const logInitiativeRoll = (character) => {
+    console.log(`${character.name} rolls ${rollInitiative(character)}`);
+};
+
+const logAttack = (attacker, defender) => {
+    if (rollAttack(attacker, defender)) {
+        console.log(`${attacker.name} hits ${defender.name}`);
+    } else {
+        console.log(`${attacker.name} misses ${defender.name}`);
     }
 };
 
-const aRoll = ndx(3,d6)
-console.log(`Bob rolls ${aRoll}`);
-console.log(`Bob rolls ${+aRoll}`);
+const logDamage = (attacker, defender) => {
+    console.log(`${attacker.name} hits ${defender.name} for ${attacker.dmg()} damage`);
+};
+
+logInitiativeRoll(goblin);
+logInitiativeRoll(cleric);
+
+logAttack(goblin, cleric);
+logAttack(cleric, goblin);
+
+logDamage(goblin, cleric);
+logDamage(cleric, goblin);
+
+/*
+    combat:
+        roll for surprise
+            surprise round (if characters are surprised)
+            {combat round}
+        roll for initiative
+        repeat
+            {combat round}
+    
+    combat round:
+        turn-wise, in initiative order
+        roll to hit
+        if hit
+            roll damage
+*/
+

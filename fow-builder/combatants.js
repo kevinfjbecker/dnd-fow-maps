5e2b519d7d9ebe9adc75c8747163d5225004a3a0
kevinfjbecker <kevinfjbecker@gmail.com>
2020-08-05 07:36:21 +0000

    Deploying to gh-pages from  @ a024bcdb937d7ad8b5384d32fe3683184a6c875c 🚀

---
+++ fow-builder/combatants.js
@@ -0,0 +1,186 @@
+const combatants = [
+    {
+        "name": "Panther",
+        "alignment": "friendly",
+        "imgSrc": "img/panther.jpg",
+        "x": 755,
+        "y": 377,
+        hidden: true
+    },
+    {
+        "name": "Wolf",
+        "alignment": "friendly",
+        "imgSrc": "img/wolf.jpg",
+        "x": 754,
+        "y": 412,
+        hidden: true
+    },
+    {
+        "name": "Belimore",
+        "alignment": "friendly",
+        "imgSrc": "img/belimore.jpg",
+        "x": 828,
+        "y": 379,
+        hidden: true
+    },
+    {
+        "name": "Cire",
+        "alignment": "friendly",
+        "imgSrc": "img/cire.jpg",
+        "x": 829,
+        "y": 410,
+        hidden: true
+    },
+    {
+        "name": "Rew",
+        "alignment": "friendly",
+        "imgSrc": "img/rew.jpg",
+        "x": 788,
+        "y": 411,
+        hidden: true
+    },
+    {
+        "name": "Speeder",
+        "alignment": "friendly",
+        "imgSrc": "img/speeder.jpg",
+        "x": 788,
+        "y": 379,
+        hidden: true
+    },
+    {
+        "name": "Random",
+        "alignment": "friendly",
+        "imgSrc": "img/random.jpeg",
+        "x": 865,
+        "y": 410,
+        hidden: true
+    },
+    {
+        "name": "Nothic",
+        "alignment": "hostile",
+        "imgSrc": "img/nothic.png",
+        "x": 377,
+        "y": 300,
+        hidden: false
+    },
+    {
+        "name": "SkeletonOne",
+        "alignment": "hostile",
+        "imgSrc": "img/skeleton.png",
+        "x": 641,
+        "y": 337,
+        hidden: true
+    },
+    {
+        "name": "SkeletonTwo",
+        "alignment": "hostile",
+        "imgSrc": "img/skeleton.png",
+        "x": 640,
+        "y": 263,
+        hidden: true
+    },
+    {
+        "name": "SkeletonThree",
+        "alignment": "hostile",
+        "imgSrc": "img/skeleton.png",
+        "x": 678,
+        "y": 264,
+        hidden: true
+    },
+    {
+        "name": "RuffianOne",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 939,
+        "y": 412,
+        hidden: true
+    },
+    {
+        "name": "RuffianTwo",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 941,
+        "y": 377,
+        hidden: true
+    },
+    {
+        "name": "RuffianThree",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 976,
+        "y": 376,
+        hidden: true
+    },
+    {
+        "name": "RuffianFour",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 153,
+        "y": 377,
+        hidden: true
+    },
+    {
+        "name": "RuffianFive",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 157,
+        "y": 306,
+        hidden: true
+    },
+    {
+        "name": "RuffianSix",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 80,
+        "y": 374,
+        hidden: true
+    },
+    {
+        "name": "RuffianSeven",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 76,
+        "y": 300,
+        hidden: true
+    },
+    {
+        "name": "BugbearOne",
+        "alignment": "hostile",
+        "imgSrc": "img/bugbear.jpg",
+        "x": 115,
+        "y": 673,
+        hidden: true
+    },
+    {
+        "name": "BugbearTwo",
+        "alignment": "hostile",
+        "imgSrc": "img/bugbear.jpg",
+        "x": 192,
+        "y": 601,
+        hidden: true
+    },
+    {
+        "name": "BugbearTree",
+        "alignment": "hostile",
+        "imgSrc": "img/bugbear.jpg",
+        "x": 189,
+        "y": 674,
+        hidden: true
+    },
+    {
+        "name": "GoblinOne",
+        "alignment": "hostile",
+        "imgSrc": "img/goblin.jpg",
+        "x": 155,
+        "y": 638,
+        hidden: true
+    },
+    {
+        "name": "Glassstaff",
+        "alignment": "boss",
+        "imgSrc": "img/glassstaff.jpeg",
+        "x": 302,
+        "y": 150,
+        hidden: true
+    }
+]

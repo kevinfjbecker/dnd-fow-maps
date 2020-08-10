2ec207faf9e0276cbf5930d491729d956244f94a
kevinfjbecker <kevinfjbecker@gmail.com>
2020-08-10 21:59:57 +0000

    Deploying to gh-pages from  @ 320489f33ba43626ae6cefce4b7527a8a5cf6745 🚀

--- fow-builder/combatants.js
+++ fow-builder/combatants.js
@@ -3,57 +3,57 @@ const combatants = [
         "name": "Panther",
         "alignment": "friendly",
         "imgSrc": "img/panther.jpg",
-        "x": 755,
-        "y": 377,
-        hidden: true
+        "x": 902,
+        "y": 598,
+        "hidden": false
     },
     {
         "name": "Wolf",
         "alignment": "friendly",
         "imgSrc": "img/wolf.jpg",
-        "x": 754,
-        "y": 412,
-        hidden: true
+        "x": 902,
+        "y": 562,
+        "hidden": false
     },
     {
         "name": "Belimore",
         "alignment": "friendly",
         "imgSrc": "img/belimore.jpg",
-        "x": 828,
-        "y": 379,
-        hidden: true
+        "x": 940,
+        "y": 671,
+        "hidden": false
     },
     {
         "name": "Cire",
         "alignment": "friendly",
         "imgSrc": "img/cire.jpg",
-        "x": 829,
-        "y": 410,
-        hidden: true
+        "x": 975,
+        "y": 672,
+        "hidden": false
     },
     {
         "name": "Rew",
         "alignment": "friendly",
         "imgSrc": "img/rew.jpg",
-        "x": 788,
-        "y": 411,
-        hidden: true
+        "x": 942,
+        "y": 562,
+        "hidden": false
     },
     {
         "name": "Speeder",
         "alignment": "friendly",
         "imgSrc": "img/speeder.jpg",
-        "x": 788,
-        "y": 379,
-        hidden: true
+        "x": 943,
+        "y": 595,
+        "hidden": false
     },
     {
         "name": "Random",
         "alignment": "friendly",
         "imgSrc": "img/random.jpeg",
-        "x": 865,
-        "y": 410,
-        hidden: true
+        "x": 938,
+        "y": 491,
+        "hidden": false
     },
     {
         "name": "Nothic",
@@ -61,7 +61,7 @@ const combatants = [
         "imgSrc": "img/nothic.png",
         "x": 377,
         "y": 300,
-        hidden: false
+        "hidden": true
     },
     {
         "name": "SkeletonOne",
@@ -69,7 +69,7 @@ const combatants = [
         "imgSrc": "img/skeleton.png",
         "x": 641,
         "y": 337,
-        hidden: true
+        "hidden": true
     },
     {
         "name": "SkeletonTwo",
@@ -77,7 +77,7 @@ const combatants = [
         "imgSrc": "img/skeleton.png",
         "x": 640,
         "y": 263,
-        hidden: true
+        "hidden": true
     },
     {
         "name": "SkeletonThree",
@@ -85,63 +85,103 @@ const combatants = [
         "imgSrc": "img/skeleton.png",
         "x": 678,
         "y": 264,
-        hidden: true
+        "hidden": true
     },
     {
         "name": "RuffianOne",
         "alignment": "hostile",
         "imgSrc": "img/ruffian.png",
-        "x": 939,
-        "y": 412,
-        hidden: true
+        "x": 603,
+        "y": 489,
+        "hidden": true
     },
     {
         "name": "RuffianTwo",
         "alignment": "hostile",
         "imgSrc": "img/ruffian.png",
-        "x": 941,
-        "y": 377,
-        hidden: true
+        "x": 643,
+        "y": 566,
+        "hidden": true
     },
     {
         "name": "RuffianThree",
         "alignment": "hostile",
         "imgSrc": "img/ruffian.png",
-        "x": 976,
-        "y": 376,
-        hidden: true
+        "x": 679,
+        "y": 488,
+        "hidden": true
     },
     {
         "name": "RuffianFour",
         "alignment": "hostile",
         "imgSrc": "img/ruffian.png",
-        "x": 153,
-        "y": 377,
-        hidden: true
+        "x": 155,
+        "y": 374,
+        "hidden": true
     },
     {
         "name": "RuffianFive",
         "alignment": "hostile",
         "imgSrc": "img/ruffian.png",
-        "x": 157,
-        "y": 306,
-        hidden: true
+        "x": 77,
+        "y": 303,
+        "hidden": true
     },
     {
         "name": "RuffianSix",
         "alignment": "hostile",
         "imgSrc": "img/ruffian.png",
-        "x": 80,
-        "y": 374,
-        hidden: true
+        "x": 755,
+        "y": 261,
+        "hidden": true
     },
     {
         "name": "RuffianSeven",
         "alignment": "hostile",
         "imgSrc": "img/ruffian.png",
-        "x": 76,
-        "y": 300,
-        hidden: true
+        "x": 754,
+        "y": 188,
+        "hidden": true
+    },
+    {
+        "name": "RuffianEight",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 77,
+        "y": 376,
+        "hidden": true
+    },
+    {
+        "name": "RuffianNine",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 154,
+        "y": 304,
+        "hidden": true
+    },
+    {
+        "name": "CommonerOne",
+        "alignment": "friendly",
+        "imgSrc": "img/girl.jpeg",
+        "x": 826,
+        "y": 334,
+        "hidden": true
+    },
+    {
+        "name": "CommonerTwo",
+        "alignment": "friendly",
+        "imgSrc": "img/woman.jpeg",
+        "x": 790,
+        "y": 335,
+        "hidden": true
+    },
+    {
+        "name": "CommonerThree",
+        "alignment": "friendly",
+        "imgSrc": "img/boy.jpeg",
+        "x": 825,
+        "y": 116,
+        "hidden": true
     },
     {
         "name": "BugbearOne",
@@ -149,7 +189,7 @@ const combatants = [
         "imgSrc": "img/bugbear.jpg",
         "x": 115,
         "y": 673,
-        hidden: true
+        "hidden": true
     },
     {
         "name": "BugbearTwo",
@@ -157,7 +197,7 @@ const combatants = [
         "imgSrc": "img/bugbear.jpg",
         "x": 192,
         "y": 601,
-        hidden: true
+        "hidden": true
     },
     {
         "name": "BugbearTree",
@@ -165,7 +205,7 @@ const combatants = [
         "imgSrc": "img/bugbear.jpg",
         "x": 189,
         "y": 674,
-        hidden: true
+        "hidden": true
     },
     {
         "name": "GoblinOne",
@@ -173,7 +213,7 @@ const combatants = [
         "imgSrc": "img/goblin.jpg",
         "x": 155,
         "y": 638,
-        hidden: true
+        "hidden": true
     },
     {
         "name": "Glassstaff",
@@ -181,6 +221,6 @@ const combatants = [
         "imgSrc": "img/glassstaff.jpeg",
         "x": 302,
         "y": 150,
-        hidden: true
+        "hidden": true
     }
 ]

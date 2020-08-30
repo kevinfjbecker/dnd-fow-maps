b5af2f1d3baf4e35985939046a0180bddcb7b14b
kevinfjbecker <kevinfjbecker@gmail.com>
2020-08-30 08:31:28 +0000

    Deploying to gh-pages from  @ 447b3681bec00553bc984eb7e8603bd8bc5e70cd 🚀

--- combatants.js
+++ combatants.js
@@ -1,187 +1,11 @@
 const combatants = [
     {
-        "name": "Panther",
-        "alignment": "friendly",
-        "imgSrc": "img/panther.jpg",
-        "x": 902,
-        "y": 598,
-        "hidden": false
-    },
-    {
-        "name": "Wolf",
-        "alignment": "friendly",
-        "imgSrc": "img/wolf.jpg",
-        "x": 902,
-        "y": 562,
-        "hidden": false
-    },
-    {
         "name": "Belimore",
         "alignment": "friendly",
         "imgSrc": "img/belimore.jpg",
-        "x": 940,
-        "y": 671,
-        "hidden": false
-    },
-    {
-        "name": "Cire",
-        "alignment": "friendly",
-        "imgSrc": "img/cire.jpg",
-        "x": 975,
-        "y": 672,
-        "hidden": false
-    },
-    {
-        "name": "Rew",
-        "alignment": "friendly",
-        "imgSrc": "img/rew.jpg",
-        "x": 942,
-        "y": 562,
-        "hidden": false
-    },
-    {
-        "name": "Speeder",
-        "alignment": "friendly",
-        "imgSrc": "img/speeder.jpg",
-        "x": 943,
-        "y": 595,
-        "hidden": false
-    },
-    {
-        "name": "Random",
-        "alignment": "friendly",
-        "imgSrc": "img/random.jpeg",
-        "x": 938,
-        "y": 491,
-        "hidden": false
-    },
-    {
-        "name": "Nothic",
-        "alignment": "hostile",
-        "imgSrc": "img/nothic.png",
-        "x": 377,
-        "y": 300,
-        "hidden": true
-    },
-    {
-        "name": "SkeletonOne",
-        "alignment": "hostile",
-        "imgSrc": "img/skeleton.png",
-        "x": 641,
-        "y": 337,
-        "hidden": true
-    },
-    {
-        "name": "SkeletonTwo",
-        "alignment": "hostile",
-        "imgSrc": "img/skeleton.png",
-        "x": 640,
-        "y": 263,
-        "hidden": true
-    },
-    {
-        "name": "SkeletonThree",
-        "alignment": "hostile",
-        "imgSrc": "img/skeleton.png",
-        "x": 678,
-        "y": 264,
-        "hidden": true
-    },
-    {
-        "name": "RuffianOne",
-        "alignment": "hostile",
-        "imgSrc": "img/ruffian.png",
-        "x": 603,
-        "y": 489,
-        "hidden": true
-    },
-    {
-        "name": "RuffianTwo",
-        "alignment": "hostile",
-        "imgSrc": "img/ruffian.png",
-        "x": 643,
-        "y": 566,
-        "hidden": true
-    },
-    {
-        "name": "RuffianThree",
-        "alignment": "hostile",
-        "imgSrc": "img/ruffian.png",
-        "x": 679,
-        "y": 488,
-        "hidden": true
-    },
-    {
-        "name": "RuffianFour",
-        "alignment": "hostile",
-        "imgSrc": "img/ruffian.png",
-        "x": 155,
-        "y": 374,
-        "hidden": true
-    },
-    {
-        "name": "RuffianFive",
-        "alignment": "hostile",
-        "imgSrc": "img/ruffian.png",
-        "x": 77,
-        "y": 303,
-        "hidden": true
-    },
-    {
-        "name": "RuffianSix",
-        "alignment": "hostile",
-        "imgSrc": "img/ruffian.png",
-        "x": 755,
-        "y": 261,
-        "hidden": true
-    },
-    {
-        "name": "RuffianSeven",
-        "alignment": "hostile",
-        "imgSrc": "img/ruffian.png",
-        "x": 754,
-        "y": 188,
-        "hidden": true
-    },
-    {
-        "name": "RuffianEight",
-        "alignment": "hostile",
-        "imgSrc": "img/ruffian.png",
-        "x": 77,
-        "y": 376,
-        "hidden": true
-    },
-    {
-        "name": "RuffianNine",
-        "alignment": "hostile",
-        "imgSrc": "img/ruffian.png",
-        "x": 154,
-        "y": 304,
-        "hidden": true
-    },
-    {
-        "name": "CommonerOne",
-        "alignment": "friendly",
-        "imgSrc": "img/girl.jpeg",
-        "x": 826,
-        "y": 334,
-        "hidden": true
-    },
-    {
-        "name": "CommonerTwo",
-        "alignment": "friendly",
-        "imgSrc": "img/woman.jpeg",
-        "x": 790,
-        "y": 335,
-        "hidden": true
-    },
-    {
-        "name": "CommonerThree",
-        "alignment": "friendly",
-        "imgSrc": "img/boy.jpeg",
-        "x": 825,
+        "x": 596,
         "y": 116,
-        "hidden": true
+        "hidden": false
     },
     {
         "name": "BugbearOne",
@@ -189,14 +13,6 @@ const combatants = [
         "imgSrc": "img/bugbear.jpg",
         "x": 115,
         "y": 673,
-        "hidden": true
-    },
-    {
-        "name": "BugbearTwo",
-        "alignment": "hostile",
-        "imgSrc": "img/bugbear.jpg",
-        "x": 192,
-        "y": 601,
         "hidden": true
     },
     {
@@ -208,12 +24,44 @@ const combatants = [
         "hidden": true
     },
     {
-        "name": "GoblinOne",
+        "name": "BugbearTwo",
         "alignment": "hostile",
-        "imgSrc": "img/goblin.jpg",
-        "x": 155,
-        "y": 638,
+        "imgSrc": "img/bugbear.jpg",
+        "x": 192,
+        "y": 601,
         "hidden": true
+    },
+    {
+        "name": "Cire",
+        "alignment": "friendly",
+        "imgSrc": "img/cire.jpg",
+        "x": 491,
+        "y": 114,
+        "hidden": false
+    },
+    {
+        "name": "CommonerOne",
+        "alignment": "friendly",
+        "imgSrc": "img/girl.jpeg",
+        "x": 604,
+        "y": 230,
+        "hidden": false
+    },
+    {
+        "name": "CommonerThree",
+        "alignment": "friendly",
+        "imgSrc": "img/boy.jpeg",
+        "x": 598,
+        "y": 186,
+        "hidden": false
+    },
+    {
+        "name": "CommonerTwo",
+        "alignment": "friendly",
+        "imgSrc": "img/woman.jpeg",
+        "x": 599,
+        "y": 153,
+        "hidden": false
     },
     {
         "name": "Glassstaff",
@@ -222,5 +70,157 @@ const combatants = [
         "x": 302,
         "y": 150,
         "hidden": true
+    },
+    {
+        "name": "GoblinOne",
+        "alignment": "hostile",
+        "imgSrc": "img/goblin.jpg",
+        "x": 155,
+        "y": 638,
+        "hidden": true
+    },
+    {
+        "name": "Nothic",
+        "alignment": "hostile",
+        "imgSrc": "img/nothic.png",
+        "x": 377,
+        "y": 300,
+        "hidden": true
+    },
+    {
+        "name": "Panther",
+        "alignment": "friendly",
+        "imgSrc": "img/panther.jpg",
+        "x": 488,
+        "y": 189,
+        "hidden": false
+    },
+    {
+        "name": "Random",
+        "alignment": "friendly",
+        "imgSrc": "img/random.jpeg",
+        "x": 642,
+        "y": 231,
+        "hidden": false
+    },
+    {
+        "name": "Rew",
+        "alignment": "friendly",
+        "imgSrc": "img/rew.jpg",
+        "x": 487,
+        "y": 152,
+        "hidden": false
+    },
+    {
+        "name": "RuffianEight",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 77,
+        "y": 376,
+        "hidden": true
+    },
+    {
+        "name": "RuffianFive",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 77,
+        "y": 303,
+        "hidden": true
+    },
+    {
+        "name": "RuffianFour",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 155,
+        "y": 374,
+        "hidden": true
+    },
+    {
+        "name": "RuffianNine",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 154,
+        "y": 304,
+        "hidden": true
+    },
+    {
+        "name": "RuffianOne",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 928,
+        "y": 326,
+        "hidden": false
+    },
+    {
+        "name": "RuffianSeven",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 526,
+        "y": 115,
+        "hidden": false
+    },
+    {
+        "name": "RuffianSix",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 558,
+        "y": 112,
+        "hidden": false
+    },
+    {
+        "name": "RuffianThree",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 891,
+        "y": 323,
+        "hidden": false
+    },
+    {
+        "name": "RuffianTwo",
+        "alignment": "hostile",
+        "imgSrc": "img/ruffian.png",
+        "x": 970,
+        "y": 317,
+        "hidden": false
+    },
+    {
+        "name": "SkeletonOne",
+        "alignment": "hostile",
+        "imgSrc": "img/skeleton.png",
+        "x": 886,
+        "y": 281,
+        "hidden": false
+    },
+    {
+        "name": "SkeletonThree",
+        "alignment": "hostile",
+        "imgSrc": "img/skeleton.png",
+        "x": 925,
+        "y": 281,
+        "hidden": false
+    },
+    {
+        "name": "SkeletonTwo",
+        "alignment": "hostile",
+        "imgSrc": "img/skeleton.png",
+        "x": 967,
+        "y": 275,
+        "hidden": false
+    },
+    {
+        "name": "Speeder",
+        "alignment": "friendly",
+        "imgSrc": "img/speeder.jpg",
+        "x": 518,
+        "y": 147,
+        "hidden": false
+    },
+    {
+        "name": "Wolf",
+        "alignment": "friendly",
+        "imgSrc": "img/wolf.jpg",
+        "x": 453,
+        "y": 189,
+        "hidden": false
     }
 ]

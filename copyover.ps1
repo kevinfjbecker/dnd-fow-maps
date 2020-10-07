c46551446889f542c60fe01aa397411de2ffef62
Kevin Becker <kevinfjbecker@gmail.com>
2020-10-07 21:00:23 +0200

    (WIP) Cragmaw Castle
    Started Cragmaw Castle (WIP)

---
+++ copyover.ps1
@@ -0,0 +1,36 @@
+
+param(
+    [string] $downloadsFolder = "~\Downloads",
+    [string] $projectFolder = "$PSScriptRoot\build",
+    [string] $filter = "*"
+)
+
+Write-Host "You are here: $projectFolder" -ForegroundColor Yellow
+
+$sourcePath = resolve-path $downloadsFolder -errorAction Stop
+$destinationPath = resolve-path $projectFolder -errorAction Stop
+$script:projectFolder = $destinationPath
+
+Write-Verbose "Monitoring $path for changes"
+
+$fsw = new-object System.IO.FileSystemWatcher $sourcePath, $filter
+$fsw.NotifyFilter = [System.IO.NotifyFilters]'FileName, LastWrite'
+
+$action = {
+
+    $watchFiles = 'geometry.js', 'combatants.js'
+    $fullPath = $event.SourceEventArgs.FullPath
+    $fileName = Split-Path $fullPath -leaf
+    $destinationRoot = $event.MessageData
+
+    if ($watchFiles.Contains($fileName)) {
+        Move-Item -Path $fullPath -Destination "$destinationRoot\$fileName" -Force
+    }
+
+}
+
+register-objectEvent $fsw Created -SourceIdentifier FileCreated -Action $action -MessageData $destinationPath
+register-objectEvent $fsw Changed -SourceIdentifier FileChanged -Action $action -MessageData $destinationPath
+
+Write-Host 'To disable this--and all other event listeners--call the following:' -ForegroundColor Yellow
+Write-Host 'Get-EventSubscriber | Unregister-Event' -ForegroundColor Green

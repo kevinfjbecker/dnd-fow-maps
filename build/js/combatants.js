e0156723cabb6cbd3920e30ff3e158362366dc6a
Kevin Becker <kevinfjbecker@gmail.com>
2020-12-05 00:55:00 +0100

    Large Monsters

--- build/js/combatants.js
+++ build/js/combatants.js
@@ -3,56 +3,109 @@ const combatants = [
         "name": "Random",
         "alignment": "friendly",
         "imgSrc": "img/random.jpeg",
-        "x": 170,
-        "y": 2772,
+        "x": 223,
+        "y": 1942,
         "hidden": false
     },
     {
         "name": "Belimore",
         "alignment": "friendly",
         "imgSrc": "img/belimore.jpg",
-        "x": 202,
-        "y": 2758,
+        "x": 197,
+        "y": 1939,
         "hidden": false
     },
     {
         "name": "Cire",
         "alignment": "friendly",
         "imgSrc": "img/cire.jpg",
-        "x": 180,
-        "y": 2736,
+        "x": 226,
+        "y": 1969,
         "hidden": false
     },
     {
         "name": "Panther",
         "alignment": "friendly",
         "imgSrc": "img/panther.jpg",
-        "x": 213,
-        "y": 2678,
+        "x": 198,
+        "y": 1969,
         "hidden": false
     },
     {
         "name": "Rew",
         "alignment": "friendly",
         "imgSrc": "img/rew.jpg",
-        "x": 193,
-        "y": 2705,
+        "x": 256,
+        "y": 1970,
         "hidden": false
     },
     {
         "name": "Speeder",
         "alignment": "friendly",
         "imgSrc": "img/speeder.jpg",
-        "x": 214,
-        "y": 2728,
+        "x": 257,
+        "y": 1942,
         "hidden": false
     },
     {
         "name": "Wolf",
         "alignment": "friendly",
         "imgSrc": "img/wolf.jpg",
-        "x": 237,
-        "y": 2698,
+        "x": 195,
+        "y": 1909,
         "hidden": false
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/nezznar.jpeg",
+        "name": "nezznar.0",
+        "x": 328.54807444739305,
+        "y": 235.4643160317837
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/spider.jfif",
+        "name": "spider.0",
+        "size": 'large',
+        "x": 389.54807444739305,
+        "y": 498.4643160317837
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/spider.jfif",
+        "name": "spider.1",
+        "size": 'large',
+        "x": 141.54807444739305,
+        "y": 496.4643160317837
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/spider.jfif",
+        "name": "spider.2",
+        "size": 'large',
+        "x": 391.54807444739305,
+        "y": 279.4643160317837
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/spider.jfif",
+        "name": "spider.3",
+        "size": 'large',
+        "x": 139.54807444739305,
+        "y": 278.4643160317837
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/ochrejelly.png",
+        "name": "ochrejelly.0",
+        "size": 'large',
+        "x": 312.50760018091364,
+        "y": 1943.488180913614
     }
 ]

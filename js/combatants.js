24fb9463d391f74abb9abdbb5abcffdb2b9c001e
kevinfjbecker <kevinfjbecker@gmail.com>
2021-03-07 23:29:42 +0000

    Deploying to gh-pages from  @ e2050f7de7f589ea67284ff23f89f9a213b26e2a 🚀

--- js/combatants.js
+++ js/combatants.js
@@ -3,56 +3,56 @@ const combatants = [
         "name": "AscydiaAdderBanklin",
         "alignment": "friendly",
         "imgSrc": "img/AscydiaAdderBanklin.png",
-        "x": 1804,
-        "y": 1477,
+        "x": 1967,
+        "y": 1400,
         "hidden": false
     },
     {
         "name": "Belvias",
         "alignment": "friendly",
         "imgSrc": "img/Belvias.jpeg",
-        "x": 1804,
-        "y": 1441,
+        "x": 1151,
+        "y": 1292,
         "hidden": false
     },
     {
         "name": "BiggsChunks",
         "alignment": "friendly",
         "imgSrc": "img/BiggsChunks.jpeg",
-        "x": 1840,
-        "y": 1440,
+        "x": 1115,
+        "y": 1292,
         "hidden": false
     },
     {
         "name": "Florry",
         "alignment": "friendly",
         "imgSrc": "img/Florry.jpeg",
-        "x": 1841,
-        "y": 1403,
+        "x": 1150,
+        "y": 1221,
         "hidden": false
     },
     {
         "name": "MoreAle",
         "alignment": "friendly",
         "imgSrc": "img/MoreAle.png",
-        "x": 1804,
-        "y": 1404,
+        "x": 1150,
+        "y": 1260,
         "hidden": false
     },
     {
         "name": "ProfessorBlue",
         "alignment": "friendly",
         "imgSrc": "img/ProfessorBlue.jpeg",
-        "x": 1840,
-        "y": 1367,
+        "x": 1077,
+        "y": 1292,
         "hidden": false
     },
     {
         "name": "SLAMMER",
         "alignment": "friendly",
         "imgSrc": "img/SLAMMER.jpeg",
-        "x": 1804,
-        "y": 1368,
+        "x": 1968,
+        "y": 1440,
         "hidden": false
     }
 ]

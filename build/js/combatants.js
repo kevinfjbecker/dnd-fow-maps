697f89304246253357bfa26a0c648d0238c50df2
Kevin Becker <kevinfjbecker@gmail.com>
2021-03-21 10:19:10 +0100

    Monsters and Exporation

--- build/js/combatants.js
+++ build/js/combatants.js
@@ -11,48 +11,153 @@ const combatants = [
         "name": "Belvias",
         "alignment": "friendly",
         "imgSrc": "img/Belvias.jpeg",
-        "x": 1151,
-        "y": 1292,
+        "x": 1467,
+        "y": 943,
         "hidden": false
     },
     {
         "name": "BiggsChunks",
         "alignment": "friendly",
         "imgSrc": "img/BiggsChunks.jpeg",
-        "x": 1115,
-        "y": 1292,
+        "x": 1439,
+        "y": 933,
         "hidden": false
     },
     {
         "name": "Florry",
         "alignment": "friendly",
         "imgSrc": "img/Florry.jpeg",
-        "x": 1150,
-        "y": 1221,
+        "x": 1405,
+        "y": 961,
         "hidden": false
     },
     {
         "name": "MoreAle",
         "alignment": "friendly",
         "imgSrc": "img/MoreAle.png",
-        "x": 1150,
-        "y": 1260,
+        "x": 1484,
+        "y": 913,
         "hidden": false
     },
     {
         "name": "ProfessorBlue",
         "alignment": "friendly",
         "imgSrc": "img/ProfessorBlue.jpeg",
-        "x": 1077,
-        "y": 1292,
+        "x": 1444,
+        "y": 1000,
         "hidden": false
     },
     {
         "name": "SLAMMER",
         "alignment": "friendly",
         "imgSrc": "img/SLAMMER.jpeg",
-        "x": 1968,
-        "y": 1440,
+        "x": 1512,
+        "y": 971,
         "hidden": false
+    },
+    {
+        "alignment": "boss",
+        "hidden": false,
+        "imgSrc": "img/kobold.jpeg",
+        "name": "Meepo",
+        "x": 1440.8419466216153,
+        "y": 966.6702855402
+    },
+    {
+        "alignment": "boss",
+        "hidden": false,
+        "imgSrc": "img/Yusdrayl.jpeg",
+        "name": "Yusdrayl",
+        "x": 336.8419466216153,
+        "y": 950.6702855402
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/kobold.jpeg",
+        "name": "kobold.0",
+        "x": 658.137681606725,
+        "y": 983.072334049753
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/kobold.jpeg",
+        "name": "kobold.1",
+        "x": 662.137681606725,
+        "y": 917.072334049753
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/kobold.jpeg",
+        "name": "kobold.2",
+        "x": 587.137681606725,
+        "y": 985.072334049753
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/kobold.jpeg",
+        "name": "kobold.3",
+        "x": 589.137681606725,
+        "y": 919.072334049753
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/kobold.jpeg",
+        "name": "kobold.4",
+        "x": 336.28587344028523,
+        "y": 910.2379679144387
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/kobold.jpeg",
+        "name": "kobold.5",
+        "x": 332.28587344028523,
+        "y": 992.2379679144387
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/skeleton.jpeg",
+        "name": "skeleton.1",
+        "x": 1596.153160198104,
+        "y": 999.1914365285152
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/skeleton.jpeg",
+        "name": "skeleton.2",
+        "x": 1662.153160198104,
+        "y": 1001.1914365285152,
+        "defeated": true
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/skeleton.jpeg",
+        "name": "skeleton.3",
+        "x": 1649.153160198104,
+        "y": 918.1914365285152
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/skeleton.jpeg",
+        "name": "skeleton.4",
+        "x": 1661.153160198104,
+        "y": 957.1914365285152
+    },
+    {
+        "alignment": "hostile",
+        "hidden": false,
+        "imgSrc": "img/skeleton.jpeg",
+        "name": "skeleton.5",
+        "x": 1601.153160198104,
+        "y": 930.1914365285152
     }
 ]

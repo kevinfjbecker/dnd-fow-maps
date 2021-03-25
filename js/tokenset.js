aeab6061f246ef27d3fb11a376bd8e44a3d639bc
kevinfjbecker <kevinfjbecker@gmail.com>
2021-03-25 23:00:23 +0000

    Deploying to gh-pages from  @ 02ff47460e864ba5c4c443e48bdf6c7a34c746e7 🚀

---
+++ js/tokenset.js
@@ -0,0 +1,4 @@
+var tokenSet = {
+    "kobold": "https://media-waterdeep.cursecdn.com/avatars/thumbnails/0/378/35/35/636252780449831836.jpeg",
+    "skeleton": "https://media-waterdeep.cursecdn.com/avatars/thumbnails/16/471/35/35/636376294572614519.jpeg"
+};

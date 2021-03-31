326a47795f18b912c450058e3fce7ad1acd2f4aa
Kevin Becker <kevinfjbecker@gmail.com>
2021-03-31 17:29:48 +0200

    Map Details
    Move map src, size and offset to data

---
+++ build/js/mapdetails.js
@@ -0,0 +1,7 @@
+var mapDetails = {
+    xlinkHref: 'https://media-waterdeep.cursecdn.com/attachments/2/797/tsc01.jpg',
+    height: '4160px',
+    width: '2008px',
+    xOffset: 0,
+    yOffset: -2080
+};

3b84fcc1820278ea8c84f5f045f97b0559cd3033
Kevin Becker <kevinfjbecker@gmail.com>
2021-05-03 00:27:40 +0200

    Exploration Update

--- build/js/combatants.js
+++ build/js/combatants.js
@@ -2,16 +2,16 @@ dndFowMap.staticState.combatants = [
   {
     "name": "Belvias",
     "alignment": "friendly",
-    "x": 544,
-    "y": 2173,
+    "x": 1584,
+    "y": 2166,
     "hidden": false,
     "tokenRef": "belvias"
   },
   {
     "name": "BiggsChunks",
     "alignment": "friendly",
-    "x": 589,
-    "y": 2174,
+    "x": 1501,
+    "y": 2049,
     "hidden": false,
     "defeated": false,
     "tokenRef": "biggschunks"
@@ -19,32 +19,32 @@ dndFowMap.staticState.combatants = [
   {
     "name": "Florry",
     "alignment": "friendly",
-    "x": 585,
-    "y": 2126,
+    "x": 1575,
+    "y": 2063,
     "hidden": false,
     "tokenRef": "florry"
   },
   {
     "name": "MoreAle",
     "alignment": "friendly",
-    "x": 588,
-    "y": 2219,
+    "x": 1624,
+    "y": 2168,
     "hidden": false,
     "tokenRef": "moreale"
   },
   {
     "name": "ProfessorBlue",
     "alignment": "friendly",
-    "x": 543,
-    "y": 2126,
+    "x": 1511,
+    "y": 2141,
     "hidden": false,
     "tokenRef": "professorblue"
   },
   {
     "name": "SLAMMER",
     "alignment": "friendly",
-    "x": 504,
-    "y": 2145,
+    "x": 1584,
+    "y": 2120,
     "hidden": false,
     "tokenRef": "slammer"
   },
@@ -52,8 +52,100 @@ dndFowMap.staticState.combatants = [
     "alignment": "friendly",
     "hidden": false,
     "name": "ErkyTimbers.0",
-    "x": 544.245297215952,
-    "y": 2217.0586907449215,
+    "x": 1628.245297215952,
+    "y": 2072.0586907449215,
     "tokenRef": "erkytimbers"
+  },
+  {
+    "alignment": "hostile",
+    "hidden": false,
+    "tokenRef": "skeleton",
+    "name": "skeleton.0",
+    "x": 871.4635514018692,
+    "y": 2117.502803738318,
+    "defeated": true
+  },
+  {
+    "alignment": "hostile",
+    "hidden": false,
+    "tokenRef": "skeleton",
+    "name": "skeleton.1",
+    "x": 535.4635514018692,
+    "y": 1947.502803738318,
+    "defeated": true
+  },
+  {
+    "alignment": "hostile",
+    "hidden": false,
+    "tokenRef": "twigblight",
+    "name": "twigblight.0",
+    "x": 500.46355140186915,
+    "y": 1915.502803738318,
+    "defeated": true
+  },
+  {
+    "alignment": "hostile",
+    "hidden": false,
+    "tokenRef": "twigblight",
+    "name": "twigblight.1",
+    "x": 752.4635514018692,
+    "y": 2188.502803738318,
+    "defeated": true
+  },
+  {
+    "alignment": "hostile",
+    "hidden": false,
+    "tokenRef": "goblin",
+    "name": "goblin.0",
+    "x": 1409.1675303622205,
+    "y": 2123.4939009806267
+  },
+  {
+    "alignment": "hostile",
+    "hidden": false,
+    "tokenRef": "goblin",
+    "name": "goblin.1",
+    "x": 1330.1675303622205,
+    "y": 2136.4939009806267
+  },
+  {
+    "alignment": "hostile",
+    "hidden": false,
+    "tokenRef": "giantrat",
+    "name": "giantrat.0",
+    "x": 1365.1675303622205,
+    "y": 2094.4939009806267
+  },
+  {
+    "alignment": "hostile",
+    "hidden": false,
+    "tokenRef": "goblin",
+    "name": "goblin.2",
+    "x": 1202.6641541669808,
+    "y": 1688.1691666038541
+  },
+  {
+    "alignment": "hostile",
+    "hidden": false,
+    "tokenRef": "goblin",
+    "name": "goblin.3",
+    "x": 1154.6641541669808,
+    "y": 1691.1691666038541
+  },
+  {
+    "alignment": "hostile",
+    "hidden": false,
+    "tokenRef": "goblin",
+    "name": "goblin.4",
+    "x": 1202.6641541669808,
+    "y": 1739.1691666038541
+  },
+  {
+    "alignment": "hostile",
+    "hidden": false,
+    "tokenRef": "firesnake",
+    "name": "firesnake.0",
+    "x": 1589.8426408307196,
+    "y": 2216.676217815817
   }
 ];
